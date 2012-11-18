f2e9604164c9c81b2b14a4443dcb460a79bac595
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-11-18 21:43:41 +0000

    Setup sites and base router
    git-svn-id: http://source.gemini.edu/software/checklists/trunk@49233 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- app/assets/app.coffee
+++ app/assets/app.coffee
@@ -4,9 +4,39 @@ App.ApplicationView = Ember.View.extend
   templateName: 'application'
 App.ApplicationController = Ember.Controller.extend()
 
+App.GS = Ember.Object.create
+  name: 'Gemini South'
+  shortcut: 'GS'
+
+App.GN = Ember.Object.create
+  name: 'Gemini North'
+  shortcut: 'GN'
+
+App.SiteSelectorController = Ember.ArrayController.extend
+  content: Ember.A([App.GS, App.GN  ])
+
+App.SiteSelectorView = Ember.View.extend
+  templateName: 'select_sites'
+
+App.SiteChecklistController = Ember.ObjectController.extend()
+
+App.SiteChecklistView = Ember.View.extend
+  templateName: 'site_checklist'
+
 App.Router = Ember.Router.extend
+  enableLogging: true,
   root: Ember.Route.extend
     index: Ember.Route.extend
-      route: '/'
+      route: '/',
+      connectOutlets: (router) ->
+        router.get('applicationController').connectOutlet('siteSelector')
+      showSite:  Ember.Route.transitionTo('site')
+    site: Ember.Route.extend
+      route: '/:site',
+      connectOutlets: (router, context) ->
+        router.get('applicationController').connectOutlet('siteChecklist', context)
+      serialize: (router, context) ->
+        console.log(context)
+        site: context.get('shortcut')
 
 App.initialize()
--- project/Build.scala
+++ project/Build.scala
@@ -3,16 +3,44 @@ import Keys._
 import PlayProject._
 
 object ApplicationBuild extends Build {
+  val emberOptions = SettingKey[Seq[String]]("ember-options")
+  val emberEntryPoints = SettingKey[PathFinder]("ember-entry-points")
 
-    val appName         = "checklists"
-    val appVersion      = "1.0-SNAPSHOT"
-
-    val appDependencies = Seq(
-      // Add your project dependencies here,
+  def EmberCompiler(ember: String, handlebars: String) = {
+    val compiler = new EmberCompiler(ember, handlebars)
+    AssetsCompiler("ember-handlebars",
+    (_ ** "*.handlebars"),
+    emberEntryPoints, {
+      (name, min) => "javascripts/" + name + ".pre" + (if (min) ".min.js" else ".js")
+    }, {
+      (handlebarsFile, options) =>
+        val (jsSource, dependencies) = compiler.compileDir(handlebarsFile, options)
+        // Any error here would be because of Handlebars, not the developer;
+        // so we don't want compilation to fail.
+        import scala.util.control.Exception._
+        val minified = catching(classOf[CompilationException])
+          .opt(play.core.jscompile.JavascriptCompiler.minify(jsSource, Some(handlebarsFile.getName)))
+        (jsSource, minified, dependencies)
+    },
+    emberOptions
     )
+  }
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val appName = "checklists"
+  val appVersion = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    //"postgresql" % "postgresql" % "9.1-901.jdbc4",
+    "rhino" % "js" % "1.7R2"
+    //"org.scalaz" %% "scalaz-core" % "6.0.4"
+  )
+
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    resolvers += "gemini repo" at "http://build.cl.gemini.edu:8081/artifactory/libs-releases",
+    emberEntryPoints <<= (sourceDirectory in Compile)(base => base / "assets" / "templates"),
+    emberOptions := Seq.empty[String]
+
+//    resourceGenerators in Compile <+= EmberCompiler(ember = "ember-1.0.0-pre.2.js", handlebars = "handlebars-1.0.rc.1.js")
+  )
 
 }
