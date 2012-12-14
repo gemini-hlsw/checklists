4b59092ae83c6b817de1c015de9f62a0d319bb2f
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2012-12-14 19:29:15 +0000

    Fixed typo
    git-svn-id: http://source.gemini.edu/software/checklists/trunk@49672 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- app/Global.scala
+++ app/Global.scala
@@ -5,7 +5,7 @@ object Global extends GlobalSettings {
   override def onStart(app: Application) {
     RegisterJodaDateMidnightConversionHelpers()
 
-    // Write tha basic data if missing
+    // Write the basic data if missing
     val sites = Site.findSites
     if (sites.isEmpty) {
       (Site(site = "GS", name = "Gemini South") :: Site(site = "GN", name = "Gemini North") :: Nil).map(Site.insertSite)
