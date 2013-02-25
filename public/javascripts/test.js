9dfe44662edd91cc3a57d20c4d24c3682dd2a6d4
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2013-02-25 01:54:27 +0000

    Some experiments
    git-svn-id: http://source.gemini.edu/software/checklists/trunk@50975 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,18 +1,8 @@
 App = Ember.Application.create({});
 
-App.CheckValues = ['', 'done', 'not done', 'NA', 'Ok', 'pending', 'not Ok']
-
 var parentsCount = 10;
 var childrenCount = 10;
 
-App.ApplicationController = Ember.Controller.extend({});
-App.ApplicationView = Ember.View.extend({
-    templateName: 'application'
-});
-
-App.IndexView = Ember.View.extend({
-    templateName: 'index'
-});
 App.IndexController = Ember.ArrayController.extend({
     load: function () {
         var content = Ember.A();
@@ -20,7 +10,9 @@ App.IndexController = Ember.ArrayController.extend({
             var children = Ember.A();
             for (j = 0; j < childrenCount; j++) {
                 var c = App.Child.create({
-                    position: j
+                    position: j,
+                    status: 'done',
+                    comment: 'it (' + i + ", " +j +")"
                 });
                 children.pushObject(c);
             }
@@ -35,12 +27,55 @@ App.IndexController = Ember.ArrayController.extend({
 });
 
 App.Child = Ember.Object.extend({
-    position: -1
+    position: -1,
+    status: null,
+    comment: null
 });
 
 App.Parent = Ember.Object.extend({
     id: 0,
     children: null
+});
+
+App.CheckValues = Ember.A(['', 'done', 'not done', 'NA', 'Ok', 'pending', 'not Ok']);
+
+
+App.ApplicationController = Ember.Controller.extend({});
+App.ApplicationView = Ember.View.extend({
+    templateName: 'application'
+});
+
+App.IndexView = Ember.View.extend({
+    templateName: 'index'
+});
+
+App.RowView = Ember.ContainerView.extend({
+    childViews: ['pView', 'sView', 'cView'],
+    tagName: 'ul',
+    choices: Ember.A(['a', 'b']),
+    positionBinding: 'context.position',
+    statusBinding: 'context.status',
+    commentBinding: 'context.comment',
+    pView: Ember.View.extend({
+        templateName: 'row_pos',
+        tagName: 'li',
+    }),
+    sView: Ember.View.extend({
+        templateName: 'row_st',
+        tagName: 'li',
+        didInsertElement: function() {
+            console.log(this.$())
+        }
+    }),
+    cView: Ember.View.extend({
+        templateName: 'row_com',
+        tagName: 'li',
+    })
+});
+
+App.RowsView = Ember.CollectionView.extend({
+  tagName: 'ul',
+  itemViewClass: App.RowView
 });
 
 App.Router = Ember.Router.extend({
