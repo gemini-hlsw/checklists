92a5d7cc5ed4212926f3d8d6315575ad383268d9
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2013-03-25 16:56:14 +0000

    Remove date field from site
    git-svn-id: http://source.gemini.edu/software/checklists/trunk@51395 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- app/models/Site.scala
+++ app/models/Site.scala
@@ -10,20 +10,19 @@ import play.api.Play.current
 import mongoContext._
 import JsonFormatters._
 
-case class Site(id: ObjectId = new ObjectId, site: String, name: String, date: DateMidnight = new DateMidnight())
+case class Site(id: ObjectId = new ObjectId, site: String, name: String)
 
 object Site extends ModelCompanion[Site, ObjectId] {
   val dao = new SalatDAO[Site, ObjectId](collection = mongoCollection("sites")) {}
 
-  def findSites: Seq[Site] = dao.find(MongoDBObject()).map(_.copy(date = DateMidnight.now())).toSeq
+  def findSites: Seq[Site] = dao.find(MongoDBObject()).toSeq
 
   def insertSite(site: Site): Site = site.copy(id = dao.insert(site, WriteConcern.Normal).getOrElse(ObjectId.get()))  // TBD Do proper validation of the error
 
   implicit object SiteFormat extends Writes[Site] {
     def writes(s: Site) = JsObject(Seq(
       "site" -> JsString(s.site),
-      "name" -> JsString(s.name),
-      "date" -> Json.toJson(s.date)
+      "name" -> JsString(s.name)
     ))
   }
 
